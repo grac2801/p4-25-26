dd1c0355982a8a612196ebc8649903b8dda4f0ef
grac2801 <oscar.gracias@icloud.com>
2025-10-05 12:07:54 -0700

    Files from chapter 2 started prior to Fall break

---
+++ ch002/L1_basicCalculations.py
@@ -0,0 +1,19 @@
+'''
+Created on Sep 18, 2025
+
+@author: ogracias
+'''
+
+print('\n\n*******************')
+print('Solve for Celsius degrees')
+# The equation is C = (F - 32) * 5/9
+fahrenheit = float(input('Enter T in Fahrenheit degrees: '))
+celsius = (fahrenheit - 32) * (5/9)
+print('The Celsius value is', celsius)
+
+
+#Make Fahrenheit from Celsius degrees
+# F = (c * 9/5) + 32
+
+
+
